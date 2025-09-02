3a9c02479e1e38bb7266b544f6f2beb5ae32ee67
kunalpawar9403 <127014248+kunalpawar9403@users.noreply.github.com>
2025-09-02 23:09:07 +0530

    Update Home.jsx

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,8 +15,8 @@ const Home = () => {
       <AboutSection />
       <ServicesSection />
       <ProductsSection />
-      <ClientsSection />
-      <Testimonials />
+     
+     
       <CtaSection />
     </>
   )
